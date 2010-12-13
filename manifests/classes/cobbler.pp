82172ad0eb363c82252223a8f960ec7896a70355
actionjack <martin@uncommonsense-uk.com>
2010-12-13 10:45:19 +0000

    Updated Readme file

--- manifests/classes/cobbler.pp
+++ manifests/classes/cobbler.pp
@@ -4,20 +4,24 @@
 #
 # Parameters:
 #   None
-#       
 #
 # Actions:
-#   Install the cobbler build server
+#   Installs the cobbler build server
 #
 # Requires:
 #   - Package["apache","xinetd"]
 #   - R.I. Pienaar's puppet-concat module
 #
 # Sample Usage:
+# node cobblerserver {
+#        include cobbler
+#        cobbler::setting{"build_reporting_email": value => "[ 'sysop@192.168.1.x' ]" }
+#        cobbler::setting{"build_reporting_smtp_server": value => "\"192.168.1.x\"" }
+#        cobbler::setting{"default_password_crypted": value => "\"$MD5SUM_PASSWORD\"" }
+#        cobbler::setting{"server": value => "192.168.1.x" }
+#        cobbler::setting{"next_server": value => "192.168.1.x" }
+#}
 #
-#
-# Variables
-
 class cobbler::install {
     $packagelist = ["cobbler","cobbler-web","pykickstart","debmirror","cman"]
     package{ $packagelist: ensure => installed }
