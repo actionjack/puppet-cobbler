fae927c675890afd21e23ab12b1ea9d9f959b67e
actionjack <martin@uncommonsense-uk.com>
2011-01-19 11:22:20 +0000

    Integrated with puppet-module tool to generate initial standard layout for reuse

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,17 @@
-import "classes/*.pp"
+# Class: cobbler
+#
+# This module manages cobbler
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class cobbler {
+
+
+}
